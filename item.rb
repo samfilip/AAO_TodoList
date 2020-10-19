c99f27642189ff624287b72f1e826ec3fb447076
Sam Filip <samjfilip@gmail.com>
2020-10-19 16:20:47 -0700

    Added initialize methods and accessor methods

---
+++ item.rb
@@ -0,0 +1,47 @@
+class Item
+    attr_reader :title, :deadline, :description
+    def valid_date?(date)
+        input = date.split('-')
+        year = input[0]
+        month = input[1].to_i
+        day = input[2].to_i
+        return false if input.length > 3 || input.length < 3
+
+        if day > 31 || day < 0
+            return false
+        end
+
+        if month < 1 || month > 12
+            return false
+        end
+
+        if year.length != 4 || year.to_i > 2020
+            return false
+        end
+        true
+ 
+    end
+
+    def initialize(title, deadline, description)
+        @title = title
+        raise "Deadline is not valid" if !valid_date?(deadline)
+        @deadline = deadline
+        @description = description
+    end
+
+    def deadline=(new_deadline)
+        raise "Deadline is not valid" if !valid_date?(deadline)
+        @deadline = new_deadline
+    end
+
+    def description=(new_description)
+        raise "Description is not valid" if !(new_description.is_a? String)
+        @description = new_description
+    end
+
+    def title=(new_title)
+        raise "Title is not valid" if !(new_description.is_a? String)
+        @title = new_title
+    end
+    
+end # Item
